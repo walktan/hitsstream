013f282ea1fe49d7903ff3b2d61247a5ced713e5
walktan <yb1.2st.50@gmail.com>
2016-06-19 22:09:16 +0900

    Delete 20160430041604_add_foreign_key_to_ranks.rb

--- db/migrate/20160430041604_add_foreign_key_to_ranks.rb
+++
@@ -1,7 +0,0 @@
-class AddForeignKeyToRanks < ActiveRecord::Migration
-  def change
-  		add_foreign_key(:ranks, :mst_dates, options: 'ON UPDATE CASCADE ON DELETE CASCADE')
-  		add_foreign_key(:ranks, :mst_genres, options: 'ON UPDATE CASCADE ON DELETE CASCADE')
-  		add_foreign_key(:ranks, :mst_musics, options: 'ON UPDATE CASCADE ON DELETE CASCADE')
-  end
-end
