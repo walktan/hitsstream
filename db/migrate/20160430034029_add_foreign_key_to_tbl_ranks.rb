c245964f18d72607efa562934891b51f921a45d9
walktan <yb1.2st.50@gmail.com>
2016-06-19 22:08:50 +0900

    Delete 20160430034029_add_foreign_key_to_tbl_ranks.rb

--- db/migrate/20160430034029_add_foreign_key_to_tbl_ranks.rb
+++
@@ -1,7 +0,0 @@
-class AddForeignKeyToTblRanks < ActiveRecord::Migration
-  def change
-  		add_foreign_key(:tbl_ranks, :mst_dates, options: 'ON UPDATE CASCADE ON DELETE CASCADE')
-  		add_foreign_key(:tbl_ranks, :mst_genres, options: 'ON UPDATE CASCADE ON DELETE CASCADE')
-  		add_foreign_key(:tbl_ranks, :mst_musics, options: 'ON UPDATE CASCADE ON DELETE CASCADE')
-  end
-end
