44414a597bd2ae36d63d4ae0d3171b7daf20c5e3
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2016-07-26 01:31:53 +0900

    Add description -Closes #42

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
+//= require_tree ../../../vendor/assets/javascripts/.
+//= require_tree ../../../vendor/assets/stylesheets/.
 //= require_tree .
 
 
