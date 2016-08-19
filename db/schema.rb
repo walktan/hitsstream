51f9202683ce9986b9510cf0647b6ebc0905149d
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2016-08-20 06:13:24 +0900

    Rename model -Fixed #52 -Fixed #54

--- db/schema.rb
+++ db/schema.rb
@@ -14,45 +14,45 @@
 ActiveRecord::Schema.define(version: 20160819080131) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "aggregate_dates", primary_key: "aggregate_date_id", force: :cascade do |t|
-    t.date     "this_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'aggregate_dates', primary_key: 'aggregate_date_id', force: :cascade do |t|
+    t.date     'this_date'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "daily_rankings", primary_key: "daily_ranking_id", force: :cascade do |t|
-    t.integer  "rank"
-    t.integer  "aggregate_date_id"
-    t.integer  "itune_id"
-    t.integer  "youtube_id"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+  create_table 'daily_rankings', primary_key: 'daily_ranking_id', force: :cascade do |t|
+    t.integer  'rank'
+    t.integer  'aggregate_date_id'
+    t.integer  'itune_id'
+    t.integer  'youtube_id'
+    t.datetime 'created_at',        null: false
+    t.datetime 'updated_at',        null: false
   end
 
-  add_index "daily_rankings", ["aggregate_date_id"], name: "index_daily_rankings_on_aggregate_date_id", using: :btree
-  add_index "daily_rankings", ["itune_id"], name: "index_daily_rankings_on_itune_id", using: :btree
-  add_index "daily_rankings", ["youtube_id"], name: "index_daily_rankings_on_youtube_id", using: :btree
+  add_index 'daily_rankings', ['aggregate_date_id'], name: 'index_daily_rankings_on_aggregate_date_id', using: :btree
+  add_index 'daily_rankings', ['itune_id'], name: 'index_daily_rankings_on_itune_id', using: :btree
+  add_index 'daily_rankings', ['youtube_id'], name: 'index_daily_rankings_on_youtube_id', using: :btree
 
-  create_table "itunes", primary_key: "itune_id", force: :cascade do |t|
-    t.string   "genre"
-    t.string   "itunes_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'itunes', primary_key: 'itune_id', force: :cascade do |t|
+    t.string   'genre'
+    t.string   'itunes_url'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "youtubes", primary_key: "youtube_id", force: :cascade do |t|
-    t.string   "title"
-    t.string   "artist"
-    t.text     "youtube_url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+  create_table 'youtubes', primary_key: 'youtube_id', force: :cascade do |t|
+    t.string   'title'
+    t.string   'artist'
+    t.text     'youtube_url'
+    t.datetime 'created_at',  null: false
+    t.datetime 'updated_at',  null: false
   end
 
-  add_foreign_key "daily_rankings", "aggregate_dates", primary_key: "aggregate_date_id"
-  add_foreign_key "daily_rankings", "itunes", primary_key: "itune_id"
-  add_foreign_key "daily_rankings", "youtubes", primary_key: "youtube_id"
+  add_foreign_key 'daily_rankings', 'aggregate_dates', primary_key: 'aggregate_date_id'
+  add_foreign_key 'daily_rankings', 'itunes', primary_key: 'itune_id'
+  add_foreign_key 'daily_rankings', 'youtubes', primary_key: 'youtube_id'
 
   create_view :view_rankings,  sql_definition: <<-SQL
       SELECT md.this_date AS target_date,
--- lib/tasks/update_daily_ranking.rb
+++ lib/tasks/update_daily_ranking.rb
@@ -1,42 +1,37 @@
-# batch
 require 'rss'
 require 'net/http'
 require 'uri'
 require 'json'
 
 API_KEY = Rails.application.secrets.youtube_api_key
-ARI_DOMAIN = 'www.googleapis.com'
+API_DOMAIN = 'www.googleapis.com'
+API_PATH= '/youtube/v3/search'
+API_URI = 'https://' + API_DOMAIN + API_PATH + '?part=snippet&maxResults=1&q='
 API_SSL_PORT = 443
 
+# batch
 class Tasks::UpdateDailyRanking
   def self.execute
     update_aggregate_dates
-
     Itune.pluck(:itunes_url, :itune_id) .each do |value|
-
       begin
         rss = RSS::Parser.parse(value[0], true)
       rescue RSS::Error
       end
-
       ranking = 0
-
       rss.items.each do |item|
-
-        titile_artist = String(item.title). \
-          gsub(/<('.*?'|'.*?'|[^''])*?>/, '').encode('UTF-8')
-
+        titile_artist = String(item.title) \
+          .gsub(/<('.*?'|'.*?'|[^''])*?>/, '').encode('UTF-8')
         artist = titile_artist.gsub(/^.*-\s/, '')
         title = titile_artist.gsub(/\s-.*$/, '')
-
         update_youtubes(artist, title)
-
         rank = DailyRanking.new
         rank.rank = ranking += 1
         rank.aggregate_date_id = AggregateDate.maximum('aggregate_date_id')
         rank.itune_id = value[1]
         begin
-          result = Youtube.where(artist: artist, title: title).pluck(:youtube_id)
+          result = Youtube.where(artist: artist, title: title) \
+            .pluck(:youtube_id)
           rank.youtube_id = result[0]
         rescue NoMethodError
           rank.youtube_id = nil
@@ -53,7 +48,6 @@ class Tasks::UpdateDailyRanking
     day.save
   end
 
-
   def self.update_youtubes(artist, title)
     if Youtube.find_by(artist: artist, title: title).nil?
       music = Youtube.new
@@ -65,10 +59,12 @@ class Tasks::UpdateDailyRanking
   end
 
   def self.get_youtube_uri(artist, title)
-    https = Net::HTTP.new(ARI_DOMAIN, API_SSL_PORT)
+    https = Net::HTTP.new(API_DOMAIN, API_SSL_PORT)
     https.use_ssl = true
     https.verify_mode = OpenSSL::SSL::VERIFY_PEER
-    res = https.request_get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=' + artist.gsub(' ', '%20') + '%20' + title.gsub(' ', '%20') + '&key=' + API_KEY)
+    uri = API_URI + artist.gsub(' ', '%20') + '%20' + title.gsub(' ', '%20') \
+            + '&key=' + API_KEY
+    res = https.request_get(uri)
     result = JSON.parse(res.body)
     begin
       return result['items'][0]['id']['videoId']
