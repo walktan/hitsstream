75f5c69e7c51f70b193a3e6cdcf549062547da42
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2016-08-21 20:32:50 +0900

    Rename model -Fixed #52 -Fixed #55

---
+++ db/migrate/20160821110819_rename_mst_genres_to_itunes.rb
@@ -0,0 +1,8 @@
+# Rename model
+class RenameMstGenresToItunes < ActiveRecord::Migration
+  def change
+    rename_column :mst_genres, :id, :itune_id
+    rename_column :mst_genres, :name, :genre
+    rename_table :mst_genres, :itunes
+  end
+end
